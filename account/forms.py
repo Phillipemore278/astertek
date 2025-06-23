973aa1402c476cf5b1500bc00991ef943f9400c9
NephygTdkingz <tdkingzict@gmail.com>
2025-06-23 10:23:36 +0100

    fixed the login logout page, corrected the phone number and email capital letters issue

--- account/forms.py
+++ account/forms.py
@@ -2,14 +2,26 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=False, max_length=30)
-    last_name = forms.CharField(required=False, max_length=30)
+    email = forms.EmailField(required=True, label="Email")
+    first_name = forms.CharField(required=False, max_length=30, label="First Name")
+    last_name = forms.CharField(required=False, max_length=30, label="Last Name")
 
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control mb-3',
+                'placeholder': field.label or field_name.capitalize()
+            })
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -19,3 +31,4 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
