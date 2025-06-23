1ec9117b8ad6bd2bd709ec9f012efe4799679f5f
NephygTdkingz <tdkingzict@gmail.com>
2025-06-23 01:51:08 +0100

    added product detail and the url to all product list mentioned

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ app_name = 'store'
 urlpatterns = [
     path('', views.store, name='store'),
     path('search/', views.product_search, name='product_search'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 
 from store.models import Product
 
@@ -32,3 +32,17 @@ def product_search(request):
         ).distinct()
 
     return render(request, 'store/product_search_results.html', {'products': products, 'query': query})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    related_products = Product.objects.filter(
+        category=product.category
+    ).exclude(id=product.id)[:5]  # limit to 4
+
+    context = {
+        'product': product,
+        'related_products': related_products
+    }
+    return render(request, 'store/product_detail.html', context)
